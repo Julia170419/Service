4d0da40c10cfe38be7ec56a24d8a5d45e50acbe6
Julia Semina <julia1704army@yandex.ru>
2026-04-15 16:47:41 +0300

    git commit

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const { resolve } = require('dns')
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    main: './main.js',
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: true,
+              esModule: false,
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|gif)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name].[contenthash][ext]'
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        options: {
+          sources: {
+            list: [
+              {
+                tag: 'img',
+                attribute: 'src',
+                type: 'src'
+              },
+              {
+                tag: 'img',
+                attribute: 'srcset',
+                type: 'srcset'
+              }
+            ]
+          }
+        }
+      },
+      {
+        test: /\.(ttf|woff|eot)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]'
+        }
+      }
+    ]
+  }
+};
